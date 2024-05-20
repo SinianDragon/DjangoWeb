568a407b07499da8ee2e165917306e32566a0b89
Sniandragon <2021211322@stu.cqupt.edu.cn>
2024-05-20 21:41:27 +0800

    first commit

---
+++ Django/myweb/static/info.js
@@ -0,0 +1,109 @@
+
+new Vue({
+    el: '#info',
+    data: {
+        name: '万光荣',
+        school: '重庆邮电大学:软件工程学院（2021/9 -- 2025/7）',
+        github: 'https://github.com/Glory-R',
+        email: 'glory947446@gmail.com',
+        researchInterests: '类脑智能、人工智能、脑科学、大数据分析、知识表征学习、计算机视觉',
+        additionalInterests: ['物理学','社会学与心理学'],
+        academicGoal: '研究出真正意义上的“类脑智能”',
+        papers: [
+            {
+              title: 'Shepard Convolutional Neural Networks',
+              conference: 'NIPS',
+              year: 2015,
+              authors: ['Jimmy SJ. Ren', 'Li Xu', 'Qiong Yan', 'Wenxiu Sun'],
+              paperUrl: 'http://www.cse.cuhk.edu.hk/~leojia/papers/mstruct_iccv15.pdf',
+              codeUrl: 'http://www.cse.cuhk.edu.hk/leojia/projects/mutualstructure/code/mutual-structure-filtering_code.zip'
+            },
+            {
+                title: 'Shepard Convolutional Neural Networks',
+                conference: 'NIPS',
+                year: 2015,
+                authors: ['Jimmy SJ. Ren', 'Li Xu', 'Qiong Yan', 'Wenxiu Sun'],
+                paperUrl: 'http://www.cse.cuhk.edu.hk/~leojia/papers/mstruct_iccv15.pdf',
+                codeUrl: 'http://www.cse.cuhk.edu.hk/leojia/projects/mutualstructure/code/mutual-structure-filtering_code.zip'
+              },
+              {
+                title: 'Shepard Convolutional Neural Networks',
+                conference: 'NIPS',
+                year: 2015,
+                authors: ['Jimmy SJ. Ren', 'Li Xu', 'Qiong Yan', 'Wenxiu Sun'],
+                paperUrl: 'http://www.cse.cuhk.edu.hk/~leojia/papers/mstruct_iccv15.pdf',
+                codeUrl: 'http://www.cse.cuhk.edu.hk/leojia/projects/mutualstructure/code/mutual-structure-filtering_code.zip'
+              },
+              {
+                title: 'Shepard Convolutional Neural Networks',
+                conference: 'NIPS',
+                year: 2015,
+                authors: ['Jimmy SJ. Ren', 'Li Xu', 'Qiong Yan', 'Wenxiu Sun'],
+                paperUrl: 'http://www.cse.cuhk.edu.hk/~leojia/papers/mstruct_iccv15.pdf',
+                codeUrl: 'http://www.cse.cuhk.edu.hk/leojia/projects/mutualstructure/code/mutual-structure-filtering_code.zip'
+              },
+          ],
+          projects: [
+            {
+              name: 'MTV-YOLO',
+              description: '基于YOLOv9提出一个多任务模型，可同时实现图像分类、分割、以及目标检测',
+              collaborators: ['Ce Liu'],
+              imageUrl: 'static/images/9.jpg'
+            },
+            {
+              name: '3MNet',
+              description: '基于YOLOv9提出的另一个多任务模型',
+              collaborators: [], // Add collaborators if any
+              githubUrl: 'https://github.com/Glory-R/LibLLIE',
+              imageUrl: 'static/images/10.jpg'
+            },
+            {
+                name: '3MNet',
+                description: '基于YOLOv9提出的另一个多任务模型',
+                collaborators: [],
+                githubUrl: 'https://github.com/Glory-R/LibLLIE',
+                imageUrl: 'static/images/10.jpg'
+              },
+              {
+                name: '3MNet',
+                description: '基于YOLOv9提出的另一个多任务模型',
+                collaborators: [],
+                githubUrl: 'https://github.com/Glory-R/LibLLIE',
+                imageUrl: 'static/images/10.jpg'
+              },
+              {
+                name: '3MNet',
+                description: '基于YOLOv9提出的另一个多任务模型',
+                collaborators: [],
+                githubUrl: 'https://github.com/Glory-R/LibLLIE',
+                imageUrl: 'static/images/10.jpg'
+              },
+              {
+                name: '3MNet',
+                description: '基于YOLOv9提出的另一个多任务模型',
+                collaborators: [],
+                githubUrl: 'https://github.com/Glory-R/LibLLIE',
+                imageUrl: 'static/images/10.jpg'
+              },
+              {
+                name: '3MNet',
+                description: '基于YOLOv9提出的另一个多任务模型',
+                collaborators: [],
+                githubUrl: 'https://github.com/Glory-R/LibLLIE',
+                imageUrl: 'static/images/10.jpg'
+              },
+
+          ],
+          experiences: [
+            '全国大学生数学竞赛',
+            '全国大学生英语竞赛',
+            '全国大学生数学建模竞赛',
+            '美国大学生数学建模竞赛',
+            '挑战者杯',
+            '创新创业创意大赛'
+          ],
+          certificates: [
+            '软件著作权 2023SR1743025'
+          ]
+        }
+      });
